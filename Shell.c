f44d0ffe3c4d4777dfdeb4c11ee5a389780d819c
Uzan <gabrielarie2000@gmail.com>
2021-02-11 15:04:15 +0100

    fpurge update

--- Shell.c
+++ Shell.c
@@ -23,7 +23,7 @@ int main(int argc, char * argv[]) {
         //demande a l'ulilisateur a tapez la cammandes
         fgets(str, sizeof(str), stdin);
         //pour les flux de sortie
-        fpurge(stdin);
+        __fpurge(stdin);
 
         //mettre une fin a la cammande saisie 
         for (k=0; k<strlen(str); k++) {
